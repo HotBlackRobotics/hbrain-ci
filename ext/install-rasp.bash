2e58061872565883a4bff001e60cfb53160f0fd1
Ludovico O. Russo <ludus.russo@gmail.com>
2017-05-23 11:29:25 +0200

    add webshell

--- ext/install-rasp.bash
+++ ext/install-rasp.bash
@@ -41,7 +41,39 @@ echo "[Service]" >> /lib/systemd/system/networking.service.d/network-pre.conf
 echo "TimeoutStartSec=1" >> /lib/systemd/system/networking.service.d/network-pre.conf
 
 
+### WebSheel from
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm --version
+nvm install v6
+npm install pm2 -g
+pm2 install pm2-webshell
+
 ### ros camera
 cd /opt/hbrain-ros/src
 git clone https://github.com/RobotWebTools/web_video_server
 git clone https://github.com/fpasteau/raspicam_node.git
+
+apt-get install ros-indigo-camera-info-manager ros-indigo-async-web-server-cpp ros-indigo-compressed-image-transport ros-indigo-theora-image-transport
+apt-get install rpi-update %% rpi-update
+
+
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_calib3d.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_calib3d.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_contrib.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_contrib.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_core.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_core.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_features2d.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_features2d.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_flann.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_flann.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_gpu.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_gpu.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_highgui.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_highgui.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_imgproc.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_imgproc.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_legacy.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_legacy.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_ml.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_ml.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_objdetect.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_objdetect.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_ocl.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_ocl.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_photo.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_photo.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_stitching.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_stitching.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_superres.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_superres.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_ts.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_ts.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_video.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_video.so.2.4.8
+ln -s /usr/lib/arm-linux-gnueabihf/libopencv_videostab.so.2.4.9 /usr/lib/arm-linux-gnueabihf/libopencv_videostab.so.2.4.8
